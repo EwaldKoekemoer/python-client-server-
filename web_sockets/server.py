478bd3b02460945e28414ce646c860a416556163
ewald <ewaldkoekemoer@gmail.com>
2024-08-04 21:42:32 +0200

    working websocket sending and graphing data (started with sorting)

--- web_sockets/server.py
+++ web_sockets/server.py
@@ -6,6 +6,32 @@ import matplotlib.pyplot as plt
 import threading
 
 
+def sort_data_graph():
+    file_path = "/home/ewald/Desktop/PycharmProjects/server_client/web_sockets/received_file.txt"
+    integers_list = []
+
+    with open(file_path, "r") as file:
+        for line in file:
+            integer = int(line.strip())
+            integers_list.append(integer)
+
+    numbers = integers_list
+    sorted_data = numbers.sort()
+
+    print(integers_list)
+
+    x = list(range(1, len(sorted_data) + 1))
+    y = sorted_data
+
+    plt.bar(x, y, label="BAR")
+    plt.xlabel("Index")
+    plt.ylabel("Value")
+    plt.title("sorted_data_graph")
+    plt.legend()
+    plt.show()
+    asyncio.sleep(5)
+    plt.close(all)
+
 def graph():
     file_path = "/home/ewald/Desktop/PycharmProjects/server_client/web_sockets/received_file.txt"
     integers_list = []
@@ -23,12 +49,11 @@ def graph():
     plt.bar(x, y, label="BAR")
     plt.xlabel("Index")
     plt.ylabel("Value")
-    plt.title("Graph Test")
+    plt.title("data_graph")
     plt.legend()
     plt.show()
-    asyncio.sleep(2)
+    asyncio.sleep(5)
     plt.close(all)
-
 
 async def receive_file(websocket, path):
     with open("received_file.txt", 'wb') as file:
@@ -37,7 +62,8 @@ async def receive_file(websocket, path):
             if data == "EOF":
                 break
             file.write(data)
-    threading.Thread(target=graph).start()
+    threading.Thread(target=graph()).start()
+    #threading.Thread(target=sort_data_graph()).start()
 
 start_server = websockets.serve(receive_file, "localhost", 8765)
 
